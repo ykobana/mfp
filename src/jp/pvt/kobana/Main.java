45f25dbf57509a4e861642208f1646c366099421
小花祐輔 <kobanayusuke@kobanayuusukenoMacBook-Pro.local>
2018-08-14 17:21:51 +0900

    remove unused files

--- src/jp/pvt/kobana/Main.java
+++
@@ -1,9 +0,0 @@
-package jp.pvt.kobana;
-
-public class Main {
-	public static void main(String args[]){
-		System.out.println("call main()");
-		MainUI mainUI = new MainUI();
-		mainUI.show();
-	}
-}
